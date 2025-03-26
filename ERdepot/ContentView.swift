70289b4379f6dd351ebea047c948fb237d3cc998
fangjunyu <fangjunyu.com@gmail.com>
2025-03-26 08:45:27 +0800

    清理所有文件内容

--- ERdepot/ContentView.swift
+++ ERdepot/ContentView.swift
@@ -9,24 +9,9 @@ import SwiftUI
 import StoreKit
 
 struct ContentView: View {
-    @State private var selectedTab = 0
     var body: some View {
-        TabView(selection: $selectedTab) {
-            // 首页视图
-            HomeView()
-                .tabItem {
-                    Image(systemName: "chart.bar") // 对应图标
-                    Text("Home") // 首页标题
-                }
-                .tag(0)
+        VStack {
             
-            // 仓库视图
-            WarehouseView()
-                .tabItem {
-                    Image(systemName: "archivebox") // 对应图标
-                    Text("Warehouse")  // 仓库标题
-                }
-                .tag(1)
         }
     }
 }
--- ERdepot/ERdepotApp.swift
+++ ERdepot/ERdepotApp.swift
@@ -9,23 +9,10 @@ import SwiftUI
 
 @main
 struct ERdepotApp: App {
-    @State private var Initializers = Initializer()
-    @ObservedObject var shared = IAPManager.shared
     
     var body: some Scene {
         WindowGroup {
-            if !Initializers.privacyPolicy {
-                privacyPolicyPage(privacyPolicy: $Initializers.privacyPolicy)
-            } else {
-                ContentView()
-                    .onAppear {
-                        print("进入Content,onAppear代码块")
-                        Task {
-                            await shared.loadProduct()
-                            await shared.handleTransactions()
-                        }
-                    }
-            }
+            ContentView()
         }
     }
 }
